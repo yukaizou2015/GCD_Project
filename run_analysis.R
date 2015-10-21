9f1c21ad1134f5ee4bafa046cdf5ec65ac509301
YukaiZou <yukaizou2012@gmail.com>
2015-10-20 21:36:47 -0700

    update

--- run_analysis.R
+++ run_analysis.R
@@ -1,8 +1,12 @@
 setwd("~/Desktop/Coursera/CleaningData/GCD_Project")
 
+# 1 Merges the training and the test sets to create one data set.
+# 2 Extracts only the measurements on the mean and standard deviation for each measurement.
+# 3 Uses descriptive activity names to name the activities in the data set.
+# 4 Appropriately labels the data set with descriptive variable names.
+
 # Read in Data
 # explore test sets
-list.files("./UCI HAR Dataset/test/")
 X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
 subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
 y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
@@ -16,8 +20,9 @@ total_acc_x_test <- read.table("./UCI HAR Dataset/test/Inertial Signals/total_ac
 total_acc_y_test <- read.table("./UCI HAR Dataset/test/Inertial Signals/total_acc_y_test.txt")
 total_acc_z_test <- read.table("./UCI HAR Dataset/test/Inertial Signals/total_acc_z_test.txt")
 
+Test <- data.frame(Activity = y_test$V1, X_test)
+
 # explore training sets
-list.files("./UCI HAR Dataset/train/Inertial Signals/")
 X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
 subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
 y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
@@ -30,6 +35,8 @@ body_gyro_z_train <- read.table("./UCI HAR Dataset/train/Inertial Signals/body_g
 total_acc_x_train <- read.table("./UCI HAR Dataset/train/Inertial Signals/total_acc_x_train.txt")
 total_acc_y_train <- read.table("./UCI HAR Dataset/train/Inertial Signals/total_acc_y_train.txt")
 total_acc_z_train <- read.table("./UCI HAR Dataset/train/Inertial Signals/total_acc_z_train.txt")
+
+Train <- data.frame(Activity = y_train$V1, X_train) # Activity: label from y_train
 
 # Export Data
 write.table(X_train, "combined.txt", row.name=FALSE)
