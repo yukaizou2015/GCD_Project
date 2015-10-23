3084044b929cdefad5ce0ac91a81f8132a610466
YukaiZou <yukaizou2012@gmail.com>
2015-10-22 23:56:03 -0700

    eliminate redundant variables

--- run_analysis.R
+++ run_analysis.R
@@ -2,25 +2,34 @@ setwd("~/Desktop/Coursera/CleaningData/GCD_Project")
 library(dplyr)
 
 # 1 Merges the training and the test sets to create one data set.
+# 2 Extracts only the measurements on the mean and standard deviation for each measurement.
+
 # Read in Data
 activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt", stringsAsFactors = FALSE)
 features <- read.table("./UCI HAR Dataset/features.txt", stringsAsFactors=FALSE)
+        # extract indexes for mean and std variables
+        ind.mean <- c(grep("mean", features$V2), grep("Mean", features$V2))
+        ind.std <- grep("std", features$V2)
 
 # read in test sets
 X_test <- read.table("./UCI HAR Dataset/test/X_test.txt"); names(X_test) <- features$V2
 subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt") %>% rename(subject = V1)
 y_test <- read.table("./UCI HAR Dataset/test/y_test.txt", stringsAsFactors = FALSE) %>% rename(activity = V1)
-Test <- data.frame(subject_test, y_test, X_test)
+Test <- data.frame(subject_test, y_test, X_test) %>% select(1:2, ind.mean+2, ind.std+2)
 
 # read in training sets
 X_train <- read.table("./UCI HAR Dataset/train/X_train.txt"); names(X_train) <- features$V2
 subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt") %>% rename(subject = V1)
 y_train <- read.table("./UCI HAR Dataset/train/y_train.txt", stringsAsFactors = FALSE) %>% rename(activity = V1)
-Train <- data.frame(subject_train, y_train, X_train) 
+Train <- data.frame(subject_train, y_train, X_train) %>% select(1:2, ind.mean+2, ind.std+2)
+head(Train[,1:6])
+combined <- full_join(Train, Test)
+# mutate(measurement = paste(subject,activity,sep="."))
+head(combined)
 
-combined <- full_join(Train, Test) %>% mutate(measurement = paste(subject,activity,sep="."))
+# Export Data
+write.table(com.mean.sd, "tidydataset.txt", row.name=FALSE)
 
-# 2 Extracts only the measurements on the mean and standard deviation for each measurement.
 com.mean <- sapply(combined, function(x) tapply(x, combined$measurement, mean))
 com.sd <- sapply(combined[,-c(1,2)], function(x) tapply(x, combined$measurement, sd))
 com.sd <- data.frame(com.mean[,1:2], com.sd)
@@ -28,8 +37,5 @@ com.mean.sd <- merge(com.mean, com.sd, by=c("subject","activity"), suffixes = c(
 
 # 3 Uses descriptive activity names to name the activities in the data set.
 com.mean.sd <- merge(activity_labels, com.mean.sd, by.x = "V1", by.y = "activity") %>% rename(activity = V2) %>% select(activity:angle.X.gravityMean..sd)
-
+head(com.mean.sd[,60:80])
 # 4 Appropriately labels the data set with descriptive variable names.
-
-# Export Data
-write.table(com.mean.sd, "combined.txt", row.name=FALSE)
