d4bd4697fd013df7f8faf1da8e055b2d5e5c62b8
YukaiZou <yukaizou2012@gmail.com>
2015-10-22 22:45:09 -0700

    1st version

--- run_analysis.R
+++ run_analysis.R
@@ -11,42 +11,30 @@ activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt", stringsAs
 features <- read.table("./UCI HAR Dataset/features.txt", stringsAsFactors=FALSE)
 
 # read in test sets
-        X_test <- read.table("./UCI HAR Dataset/test/X_test.txt"); names(X_test) <- features$V2
-        subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt") %>% rename(subject = V1)
-        y_test <- read.table("./UCI HAR Dataset/test/y_test.txt", stringsAsFactors = FALSE) %>% rename(activity = V1)
+X_test <- read.table("./UCI HAR Dataset/test/X_test.txt"); names(X_test) <- features$V2
+subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt") %>% rename(subject = V1)
+y_test <- read.table("./UCI HAR Dataset/test/y_test.txt", stringsAsFactors = FALSE) %>% rename(activity = V1)
 
-        body_acc_x_test <- read.table("./UCI HAR Dataset/test/Inertial Signals/body_acc_x_test.txt")
-        body_acc_y_test <- read.table("./UCI HAR Dataset/test/Inertial Signals/body_acc_y_test.txt")
-        body_acc_z_test <- read.table("./UCI HAR Dataset/test/Inertial Signals/body_acc_z_test.txt")
-        body_gyro_x_test <- read.table("./UCI HAR Dataset/test/Inertial Signals/body_gyro_x_test.txt")
-        body_gyro_y_test <- read.table("./UCI HAR Dataset/test/Inertial Signals/body_gyro_y_test.txt")
-        body_gyro_z_test <- read.table("./UCI HAR Dataset/test/Inertial Signals/body_gyro_z_test.txt")
-        total_acc_x_test <- read.table("./UCI HAR Dataset/test/Inertial Signals/total_acc_x_test.txt")
-        total_acc_y_test <- read.table("./UCI HAR Dataset/test/Inertial Signals/total_acc_y_test.txt")
-        total_acc_z_test <- read.table("./UCI HAR Dataset/test/Inertial Signals/total_acc_z_test.txt")
-
-Test <- data.frame(subject_test, y_test, X_test); Test <- merge(activity_labels, Test, by.x = "V1", by.y = "activity") %>% rename(activity.label = V1, activity = V2) %>% arrange(subject) %>% mutate(subset = "Test")
-tail(Test)
+Test <- data.frame(subject_test, y_test, X_test)
 
 # read in training sets
-        X_train <- read.table("./UCI HAR Dataset/train/X_train.txt"); names(X_train) <- features$V2
-        subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt") %>% rename(subject = V1)
-        y_train <- read.table("./UCI HAR Dataset/train/y_train.txt", stringsAsFactors = FALSE) %>% rename(activity = V1)
+X_train <- read.table("./UCI HAR Dataset/train/X_train.txt"); names(X_train) <- features$V2
+subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt") %>% rename(subject = V1)
+y_train <- read.table("./UCI HAR Dataset/train/y_train.txt", stringsAsFactors = FALSE) %>% rename(activity = V1)
 
-body_acc_x_train <- read.table("./UCI HAR Dataset/train/Inertial Signals/body_acc_x_train.txt")
-body_acc_y_train <- read.table("./UCI HAR Dataset/train/Inertial Signals/body_acc_y_train.txt")
-body_acc_z_train <- read.table("./UCI HAR Dataset/train/Inertial Signals/body_acc_z_train.txt")
-body_gyro_x_train <- read.table("./UCI HAR Dataset/train/Inertial Signals/body_gyro_x_train.txt")
-body_gyro_y_train <- read.table("./UCI HAR Dataset/train/Inertial Signals/body_gyro_y_train.txt")
-body_gyro_z_train <- read.table("./UCI HAR Dataset/train/Inertial Signals/body_gyro_z_train.txt")
-total_acc_x_train <- read.table("./UCI HAR Dataset/train/Inertial Signals/total_acc_x_train.txt")
-total_acc_y_train <- read.table("./UCI HAR Dataset/train/Inertial Signals/total_acc_y_train.txt")
-total_acc_z_train <- read.table("./UCI HAR Dataset/train/Inertial Signals/total_acc_z_train.txt")
+Train <- data.frame(subject_train, y_train, X_train) 
+head(Train[,1:6])
+combined <- full_join(Train, Test) %>% mutate(measurement = paste(subject,activity,sep="."))
 
-Train <- data.frame(subject_train, y_train, X_train); Train <- merge(activity_labels, Train, by.x = "V1", by.y = "activity") %>% rename(activity.label = V1, activity = V2) %>% arrange(subject) %>% mutate(subset = "Train")
-tail(Train)
+# combined <- merge(activity_labels, combined, by.x = "V1", by.y = "activity") %>% rename(activity.label = V1, activity = V2) %>% arrange(subject) %>% mutate(subset = "Train")
+# combine train and test -> extract mean and std -> merge activity label -> select out label -> save data
+com.mean <- sapply(combined, function(x) tapply(x, combined$measurement, mean))
+com.sd <- sapply(combined[,-c(1,2)], function(x) tapply(x, combined$measurement, sd))
+com.sd <- data.frame(com.mean[,1:2], com.sd)
+head(com.sd[,1:6])
+com.mean.sd <- merge(com.mean, com.sd, by=c("subject","activity"), suffixes = c(".mean", ".sd"))
+head(com.mean.sd[,1000:1006])
 
-combined <- full_join(Train, Test)
 
 # Export Data
 write.table(combined, "combined.txt", row.name=FALSE)
